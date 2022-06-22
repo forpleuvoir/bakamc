8549456dc4151764d74217a85b12dfb8e2e24e5f
forpleuvoir <695801070@qq.com>
2022-06-22 22:49:42 +0800

    小修改

--- common/src/main/kotlin/org/bakamc/common/network/http/HttpUtil.kt
+++ common/src/main/kotlin/org/bakamc/common/network/http/HttpUtil.kt
@@ -12,25 +12,47 @@ import java.util.function.Consumer
 
 object HttpUtil {
 
-	fun <T> get(uri: String, bodyHandler: BodyHandler<T>, apply: Consumer<T>, vararg params: Pair<String, String>) {
+	fun <T> get(
+		uri: String,
+		bodyHandler: BodyHandler<T>,
+		apply: Consumer<T>,
+		headers: List<Pair<String, String>>?,
+		vararg params: Pair<String, String>,
+	) {
+		val url = uri.run {
+			val str = StringBuilder(this)
+			str.append("?")
+			params.forEachIndexed { index, pair ->
+				str.append(pair.first, "=", pair.second)
+				if (index != params.size - 1) str.append("&")
+			}
+			str.toString()
+		}
+		val header = LinkedList<String>().apply {
+			headers?.forEach {
+				this.addLast(it.first)
+				this.addLast(it.second)
+			}
+		}
 		val client = HttpClient.newHttpClient()
 		val request = HttpRequest.newBuilder()
-			.uri(URI.create(uri))
+			.uri(URI.create(url))
+			.apply {
+				if (!header.isEmpty()) headers(*header.toTypedArray())
+			}
 			.build()
-		client.sendAsync(request, bodyHandler)
-			.thenApply(HttpResponse<T>::body)
-			.thenAccept(apply)
-			.get()
+		apply.accept(client.send(request, bodyHandler).body())
 	}
 
-	fun get(uri: String, apply: Consumer<String>, vararg params: Pair<String, String>) {
-		get(uri, BodyHandlers.ofString(), apply, params = params)
+	fun get(uri: String, apply: Consumer<String>, headers: List<Pair<String, String>>? = null, vararg params: Pair<String, String>) {
+		get(uri, BodyHandlers.ofString(), apply, headers, params = params)
 	}
 
-	fun get(uri: String, apply: Consumer<String>, params: JsonObject) {
+	fun get(uri: String, apply: Consumer<String>, headers: List<Pair<String, String>>? = null, params: JsonObject) {
 		get(
 			uri = uri,
 			apply = apply,
+			headers = headers,
 			params = LinkedList<Pair<String, String>>().apply { params.entrySet().forEach { add(it.key to it.value.toString()) } }.toTypedArray()
 		)
 	}
